f63f1bedac75c4d23f5409ab303cf00860ccb620
ridoyislamnasim <ridoyislamnasim@gmail.com>
2025-12-08 12:45:55 +0600

    build solve

---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+declare module "shortid";
+declare module "geoip-lite";
+declare module "crypto-js";
+declare module "otp-generator";
+// declare module "md5";
+// declare module "jsonwebtoken";
+// declare module "bcryptjs";
+// declare module "express-async-handler"          
