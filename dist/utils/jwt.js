1231e991b516c8da55b82dc077547a8a78162c68
ridoyislamnasim <ridoyislamnasim@gmail.com>
2025-12-08 13:22:05 +0600

    Fix: Add missing TypeScript types for deployment 00

--- dist/utils/jwt.js
+++ dist/utils/jwt.js
@@ -1,4 +1,6 @@
 "use strict";
+// import jwt from 'jsonwebtoken';
+// import config from '../config/config';
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,11 +9,45 @@ exports.generateAccessToken = generateAccessToken;
 exports.generateRefreshToken = generateRefreshToken;
 exports.verifyAccessToken = verifyAccessToken;
 exports.verifyRefreshToken = verifyRefreshToken;
+// export function generateAccessToken(payload: object): string {
+//   // console.log('Generating access token with payload:', config);
+//   // console.log('Generating access token with payload:', config.jwtAccessSecretKey);
+//   if (!config.jwtAccessSecretKey) {
+//     throw new Error('JWT access secret key is not defined');
+//   }
+//   return jwt.sign(payload, config.jwtAccessSecretKey, { expiresIn: '30d' });
+// }
+// export function generateRefreshToken(payload: object): string {
+//   if (!config.jwtRefreshSecretKey) {
+//     throw new Error('JWT refresh secret key is not defined');
+//   }
+//   return jwt.sign(payload, config.jwtRefreshSecretKey, { expiresIn: '365d' });
+// }
+// export function verifyAccessToken(token: string): Promise<object | string> {
+//   return new Promise((resolve, reject) => {
+//     if (!config.jwtAccessSecretKey) {
+//       return reject(new Error('JWT access secret key is not defined'));
+//     }
+//     jwt.verify(token, config.jwtAccessSecretKey, (err, payload) => {
+//       if (err) return reject(err);
+//       resolve(payload!);
+//     });
+//   });
+// }
+// export function verifyRefreshToken(token: string): Promise<object | string> {
+//   return new Promise((resolve, reject) => {
+//     if (!config.jwtRefreshSecretKey) {
+//       return reject(new Error('JWT refresh secret key is not defined'));
+//     }
+//     jwt.verify(token, config.jwtRefreshSecretKey, (err, payload) => {
+//       if (err) return reject(err);
+//       resolve(payload!);
+//     });
+//   });
+// }
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const config_1 = __importDefault(require("../config/config"));
 function generateAccessToken(payload) {
-    // console.log('Generating access token with payload:', config);
-    // console.log('Generating access token with payload:', config.jwtAccessSecretKey);
     if (!config_1.default.jwtAccessSecretKey) {
         throw new Error('JWT access secret key is not defined');
     }
