effc078f6064d2a00e9d94c719c92a63e29bc01e
ridoyislamnasim <ridoyislamnasim@gmail.com>
2025-12-09 13:17:28 +0600

    upload  error solve for deploy 01

--- src/middleware/upload/upload.ts
+++ src/middleware/upload/upload.ts
@@ -4,32 +4,32 @@ import { Request } from 'express';
 const storage = multer.memoryStorage();
 
 const allowedMimes = [
-  'image/png',
-  'image/webp',
-  'image/jpg',
-  'image/jpeg',
-  'image/gif',
-  'image/avif',
-  'image/bmp',
-  'image/svg+xml',
-  'application/pdf',
-  // video type allow
-  'video/mp4',
-  'video/webm',
-  'video/ogg',
-  'video/mpeg',
+    'image/png',
+    'image/webp',
+    'image/jpg',
+    'image/jpeg',
+    'image/gif',
+    'image/avif',
+    'image/bmp',
+    'image/svg+xml',
+    'application/pdf',
+    // video type allow
+    'video/mp4',
+    'video/webm',
+    'video/ogg',
+    'video/mpeg',
 ];
 
 export const upload = multer({
-  storage,
-  limits: {
-    fileSize: 1024 * 1024 * 100, // 100MB
-  },
-  fileFilter: (req: Request, file: Express.Multer.File, cb: (error: Error | null, acceptFile: boolean) => void) => {
-    if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error(`Unsupported file type: ${file.mimetype}`), false);
-    }
-  },
+    storage,
+    limits: {
+        fileSize: 1024 * 1024 * 100, // 100MB
+    },
+    fileFilter: (req: Request, file: Express.Multer.File, cb: (error: Error | null, acceptFile: boolean) => void) => {
+        if (allowedMimes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error(`Unsupported file type: ${file.mimetype}`), false);
+        }
+    },
 });
