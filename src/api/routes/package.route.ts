dcb651a0e1d112576f767d7a6567b8176c45568c
masudfcs1 <masudfcs1@gmail.com>
2026-01-05 18:34:00 +0600

    package route update

--- src/api/routes/package.route.ts
+++ src/api/routes/package.route.ts
@@ -1,23 +1,23 @@
 import { Router } from "express";
-import controller from "../../modules/package/package.controller";
 import jwtAuth from "../../middleware/auth/jwtAuth";
+import packageController from "../../modules/package/package.controller";
 
 const PackageRoute = Router();
 PackageRoute.use(jwtAuth());
 
 PackageRoute.route("/")
-  .post(jwtAuth(), controller.createPackage)
-  .get(controller.getAllPackages);
+  .post(jwtAuth(), packageController.createPackage)
+  .get(packageController.getAllPackages);
 
-PackageRoute.get("/pagination", controller.getPackagesWithPagination);
-PackageRoute.get("/by-type/:type", controller.getPackagesByType);
-PackageRoute.get("/by-status/:status", controller.getPackagesByStatus);
+PackageRoute.get("/pagination", packageController.getPackagesWithPagination);
+PackageRoute.get("/by-type/:type", packageController.getPackagesByType);
+PackageRoute.get("/by-status/:status", packageController.getPackagesByStatus);
 
 PackageRoute.route("/:id")
-  .get(controller.getSinglePackage)
-  .put(controller.updatePackage)
-  .delete(controller.deletePackage);
+  .get(packageController.getSinglePackage)
+  .put(packageController.updatePackage)
+  .delete(packageController.deletePackage);
 
-PackageRoute.put("/:id/status", controller.updatePackageStatus);
+PackageRoute.put("/:id/status", packageController.updatePackageStatus);
 
 export default PackageRoute;
